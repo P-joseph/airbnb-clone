49c16478ab5eebc7be4d0bc19b8d8efbf03ed02d
Joseph.P <quep1001@gmail.com>
2021-12-03 23:19:31 +0900

    related name added for class Room

--- rooms/admin.py
+++ rooms/admin.py
@@ -50,6 +50,7 @@ class RoomAdmin(admin.ModelAdmin):
         "check_in",
         "check_out",
         "instant_book",
+        "count_amenities"
     )
 
     list_filter = (
@@ -69,6 +70,10 @@ class RoomAdmin(admin.ModelAdmin):
         "facilities",
         "house_rules",
     )
+
+    def count_amenities(self, obj):
+        return obj.amenities.count()
+
 
 @admin.register(models.Photo)
 class PhotoAdmin(admin.ModelAdmin):
