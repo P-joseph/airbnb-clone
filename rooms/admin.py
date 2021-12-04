c260214a1c352e247f768f6f5c6ab5d23ced1133
Joseph.P <quep1001@gmail.com>
2021-12-04 19:02:35 +0900

    Finishing the Room admin & create func Review Average

--- rooms/admin.py
+++ rooms/admin.py
@@ -6,8 +6,13 @@ from . import models
 class ItemAdmin(admin.ModelAdmin):
 
     """ Item Admin Definition """
+    list_display = (
+        "name",
+        "used_by",
+    )
 
-    pass
+    def used_by(self, obj):
+        return obj.rooms.count()
 
 
 @admin.register(models.Room)
@@ -50,7 +55,9 @@ class RoomAdmin(admin.ModelAdmin):
         "check_in",
         "check_out",
         "instant_book",
-        "count_amenities"
+        "count_amenities",
+        "count_photos",
+        "total_rating",
     )
 
     list_filter = (
@@ -73,6 +80,9 @@ class RoomAdmin(admin.ModelAdmin):
 
     def count_amenities(self, obj):
         return obj.amenities.count()
+
+    def count_photos(self, obj):
+        return obj.photos.count()
 
 
 @admin.register(models.Photo)
