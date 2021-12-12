407180ef4838960043fc3e9c892444595df80446
Joseph.P <quep1001@gmail.com>
2021-12-12 23:14:02 +0900

    Added to search.html & rooms.views

--- rooms/views.py
+++ rooms/views.py
@@ -35,8 +35,10 @@ def search(request):
     bedrooms = int(request.GET.get("bedrooms", 0))
     beds = int(request.GET.get("beds", 0))
     baths = int(request.GET.get("baths", 0))
-    selected_amenities = request.GET.get("amenities")
-    selected_facilities = request.GET.get("facilities")
+    instant = request.GET.get("instant", False)
+    super_host = request.GET.get("super_host", False)
+    selected_amenities = request.GET.getlist("amenities")
+    selected_facilities = request.GET.getlist("facilities")
 
     form = {
         "city": city,
@@ -47,6 +49,10 @@ def search(request):
         "bedrooms": bedrooms,
         "beds": beds,
         "baths": baths,
+        "selected_amenities": selected_amenities,
+        "selected_facilities": selected_facilities,
+        "instant": instant,
+        "super_host": super_host,
     }
 
     room_types = models.RoomType.objects.all()
