98c91bdfecf327c76e64016684fc716ccf374ea8
Joseph.P <quep1001@gmail.com>
2021-12-27 23:32:53 +0900

    Added a class to deny access to invalid pages. (EmailLoginOnlyView, LoggedInOnlyView)

--- users/views.py
+++ users/views.py
@@ -15,7 +15,6 @@ class LoginView(mixins.LoggedOutOnlyView, FormView):
 
     template_name = "users/login.html"
     form_class = forms.LoginForm
-    success_url = reverse_lazy("core:home")
 
     def form_valid(self, form):
         email = form.cleaned_data.get("email")
@@ -24,6 +23,13 @@ class LoginView(mixins.LoggedOutOnlyView, FormView):
         if user is not None:
             login(self.request, user)
         return super().form_valid(form)
+
+    def get_success_url(self):
+        next_arg = self.request.GET.get("next")
+        if next_arg is not None:
+            return next_arg
+        else:
+            return reverse("core:home")
 
 def log_out(request):
     messages.info(request, f"See you later {request.user.first_name}")
@@ -195,7 +201,7 @@ class UserProfileView(DetailView):
     model = models.User
     context_object_name = "user_obj"
 
-class UpdateProfileView(SuccessMessageMixin, UpdateView):
+class UpdateProfileView(mixins.LoggedInOnlyView, SuccessMessageMixin, UpdateView):
 
     model = models.User
     template_name = "users/update-profile.html"
@@ -221,7 +227,12 @@ class UpdateProfileView(SuccessMessageMixin, UpdateView):
         form.fields["birthdate"].widget.attrs = {'placeholder': "Birthdate"}
         return form
 
-class UpdatePasswordView(SuccessMessageMixin, PasswordChangeView):
+class UpdatePasswordView(
+    mixins.EmailLoginOnlyView,
+    mixins.LoggedInOnlyView,
+    SuccessMessageMixin,
+    PasswordChangeView
+):
 
     template_name = "users/update-password.html"
     success_message = "Password Updated"
