94d6051297e4a5630bbc69f9bd1935525e6b422b
Joseph.P <quep1001@gmail.com>
2022-01-22 22:02:11 +0900

    Deploy Test | container commands

--- .platform/hooks/postdeploy/01_migrate.sh
+++ .platform/hooks/postdeploy/01_migrate.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 
-source "$PYTHONPATH/activate" && {
+source /var/app/venv/*/bin/activate
+cd /var/app/staging
 
-    if [[ $EB_IS_COMMAND_LEADER == "true" ]];
-    then
-        # log which migrations have already been applied
-        python manage.py showmigrations;
-
-        # migrate
-        python manage.py migrate --noinput;
-    else
-        echo "this instance is NOT the leader";
-    fi
-
-}
+python manage.py makemigrations
+python manage.py migrate
+python manage.py createfirstsuperuser
+python manage.py collectstatic --noinput
