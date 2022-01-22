5aef2a30f5fad93ecf03cbb3caca63dd3bc66dc9
Joseph.P <quep1001@gmail.com>
2022-01-23 00:43:32 +0900

    Deploy Test | container commands

--- config/wsgi.py
+++ config/wsgi.py
@@ -8,9 +8,20 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "airbnb-clone.config.settings")
 
-application = get_wsgi_application()
+if os.environ.get("DJANGO_SETTINGS_MODULE") == "airbnb-clone.config.settings":
+    from raven.contrib.django.raven_compat.middleware.wsgi import Sentry
+    application = Sentry(get_wsgi_application())
+else:
+    get_wsgi_application()
+
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+#
+# application = get_wsgi_application()
