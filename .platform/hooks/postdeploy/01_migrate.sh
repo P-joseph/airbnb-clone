787a42cf8a4a068954076e9db10b5d24cdbd38ee
Joseph.P <quep1001@gmail.com>
2022-01-22 23:21:37 +0900

    Deploy Test | container commands

--- .platform/hooks/postdeploy/01_migrate.sh
+++ .platform/hooks/postdeploy/01_migrate.sh
@@ -7,3 +7,7 @@ python manage.py makemigrations
 python manage.py migrate
 python manage.py createsuperuser
 python manage.py collectstatic --noinput
+python manage.py seed_amenities
+python manage.py seed_facilities
+python manage.py seed_users --number 20
+python manage.py seed_rooms --number 30
