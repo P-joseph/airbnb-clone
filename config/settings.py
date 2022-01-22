3c7b471e385e8a742e05faf1c66919065116043d
Joseph.P <quep1001@gmail.com>
2022-01-22 22:56:38 +0900

    Deploy Test | container commands

--- config/settings.py
+++ config/settings.py
@@ -105,10 +105,10 @@ else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
-            'HOST': os.environ.get("RDS_HOST"),
-            'NAME': os.environ.get("RDS_NAME"),
-            'USER': os.environ.get("RDS_USER"),
-            'PASSWORD': os.environ.get("RDS_PASSWORD"),
+            'HOST': os.environ["RDS_HOST"],
+            'NAME': os.environ["RDS_NAME"],
+            'USER': os.environ["RDS_USER"],
+            'PASSWORD': os.environ["RDS_PASSWORD"],
             'PORT': "5432",
         }
     }
