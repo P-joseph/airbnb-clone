f71502e24a819c150b029709c3a6a3747deb8ef6
Joseph.P <quep1001@gmail.com>
2022-01-23 17:03:56 +0900

    Deploy Test | container commands

--- .platform/hooks/postdeploy/01_migrate.sh
+++ .platform/hooks/postdeploy/01_migrate.sh
@@ -1,23 +1,3 @@
-##!/bin/bash
-#
-#source /var/app/venv/*/bin/activate
-#cd /var/app/staging
-#
-#python manage.py collectstatic --noinput
-#python manage.py seed_amenities
-#python manage.py seed_facilities
-#python manage.py seed_users --number 20
-#python manage.py seed_rooms --number 30
-#python manage.py createsuperuser
-#python manage.py migrate --run --syncdb
-#python manage.py makemigrations user
-#python manage.py makemigrations web
-#python manage.py makemigrations
-#python manage.py migrate
-#django-admin.py migrate
-
-#!/bin/bash
-
 source "$PYTHONPATH/activate" && {
 # log which migrations have already been applied
 python manage.py showmigrations;
