62ed3c7c6b5a3ea57c8b02280e288bda97ca716f
Joseph.P <quep1001@gmail.com>
2022-01-23 16:29:11 +0900

    Deploy Test | container commands

--- .platform/hooks/postdeploy/01_migrate.sh
+++ .platform/hooks/postdeploy/01_migrate.sh
@@ -8,7 +8,7 @@ python manage.py seed_amenities
 python manage.py seed_facilities
 python manage.py seed_users --number 20
 python manage.py seed_rooms --number 30
+python manage.py createsuperuser
 python manage.py makemigrations
 python manage.py migrate
-python manage.py createsuperuser
 django-admin.py migrate
