ba27011d1a659f6e74488a36d38dc1b28454d5ed
Joseph.P <quep1001@gmail.com>
2022-01-23 16:42:22 +0900

    Deploy Test | container commands

--- .platform/hooks/postdeploy/01_migrate.sh
+++ .platform/hooks/postdeploy/01_migrate.sh
@@ -1,17 +1,34 @@
+##!/bin/bash
+#
+#source /var/app/venv/*/bin/activate
+#cd /var/app/staging
+#
+#python manage.py collectstatic --noinput
+#python manage.py seed_amenities
+#python manage.py seed_facilities
+#python manage.py seed_users --number 20
+#python manage.py seed_rooms --number 30
+#python manage.py createsuperuser
+#python manage.py migrate --run --syncdb
+#python manage.py makemigrations user
+#python manage.py makemigrations web
+#python manage.py makemigrations
+#python manage.py migrate
+#django-admin.py migrate
+
 #!/bin/bash
 
-source /var/app/venv/*/bin/activate
-cd /var/app/staging
+source "$PYTHONPATH/activate" && {
+    
+    if [[ $EB_IS_COMMAND_LEADER == "true" ]];
+    then
+        # log which migrations have already been applied
+        python manage.py showmigrations;
 
-python manage.py collectstatic --noinput
-python manage.py seed_amenities
-python manage.py seed_facilities
-python manage.py seed_users --number 20
-python manage.py seed_rooms --number 30
-python manage.py createsuperuser
-python manage.py migrate --run --syncdb
-python manage.py makemigrations user
-python manage.py makemigrations web
-python manage.py makemigrations
-python manage.py migrate
-django-admin.py migrate
+        # migrate
+        python manage.py migrate --noinput;
+    else
+        echo "this instance is NOT the leader";
+    fi
+
+}
