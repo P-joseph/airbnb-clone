949c4c9119ee7251ce3682fd9e6c5b2a1113851a
Joseph.P <quep1001@gmail.com>
2022-01-24 18:07:48 +0900

    Deploy Test Restart

--- .platform/hooks/predeploy/01_migrations.sh
+++ .platform/hooks/predeploy/01_migrations.sh
@@ -4,17 +4,7 @@ source /var/app/venv/*/bin/activate
 cd /var/app/staging
 
 python manage.py makemigrations
-python manage.py migrate config
-python manage.py migrate conversations
-python manage.py migrate core
-python manage.py migrate listsq
-python manage.py migrate reservations
-python manage.py migrate reviews
-python manage.py migrate rooms
-python manage.py migrate users
-python manage.py dbshell
-DROP INDEX documents_document_content_aa150741_like;
-DROP INDEX documents_document_content_aa150741;
+python manage.py migrate
 python manage.py createsuperuser
 python manage.py seed_amenities
 python manage.py seed_facilities
