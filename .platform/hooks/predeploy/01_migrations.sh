130b8f079331b6fc5306c7d52a36fb7d54abdb36
Joseph.P <quep1001@gmail.com>
2022-01-24 16:12:37 +0900

    Deploy Test Restart

--- .platform/hooks/predeploy/01_migrations.sh
+++ .platform/hooks/predeploy/01_migrations.sh
@@ -4,6 +4,7 @@ source /var/app/venv/*/bin/activate
 cd /var/app/staging
 
 python manage.py makemigrations
+python manage.py migrate users
 python manage.py migrate
 python manage.py createsuperuser
 python manage.py seed_amenities
