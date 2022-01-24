6662ad54053da6c1b7d5f97c53121e4e268984e9
Joseph.P <quep1001@gmail.com>
2022-01-24 17:19:03 +0900

    Deploy Test Restart

--- .platform/hooks/predeploy/01_migrations.sh
+++ .platform/hooks/predeploy/01_migrations.sh
@@ -12,6 +12,9 @@ python manage.py migrate reservations
 python manage.py migrate reviews
 python manage.py migrate rooms
 python manage.py migrate users
+python manage.py dbshell
+DROP INDEX documents_document_content_aa150741_like;
+DROP INDEX documents_document_content_aa150741;
 python manage.py createsuperuser
 python manage.py seed_amenities
 python manage.py seed_facilities
