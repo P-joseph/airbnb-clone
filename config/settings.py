ae0f9b790c18bbd20955447c5213f8fcdd8c8f55
Joseph.P <quep1001@gmail.com>
2022-01-26 21:57:56 +0900

    Server Error Test

--- config/settings.py
+++ config/settings.py
@@ -92,7 +92,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-if not DEBUG:
+if DEBUG:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
