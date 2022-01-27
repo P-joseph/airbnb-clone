b7af15cc9f1d5ac22dee89168362f8f6b2d9577d
Joseph.P <quep1001@gmail.com>
2022-01-27 20:31:57 +0900

    Add Test Seed of Data

--- .platform/hooks/predeploy/05_createseed.sh
+++ .platform/hooks/predeploy/05_createseed.sh
@@ -5,3 +5,6 @@ cd /var/app/staging
 
 python3 manage.py seed_amenities
 python3 manage.py seed_facilities
+python3 manage.py seed_users --number 20
+python3 manage.py seed_rooms --number 40
+python3 manage.py seed_reviews --number 100
