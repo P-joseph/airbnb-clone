a723466b99ea10e2e815d3478180b84054e8ae7d
Joseph.P <quep1001@gmail.com>
2022-02-04 16:45:50 +0900

    Connecting try to 'S3 storage'

--- config/settings.py
+++ config/settings.py
@@ -180,18 +180,13 @@ LANGUAGE_COOKIE_NAME = "django_language"
 
 # Sentry
 
-
 if DEBUG:
 
     DEFAULT_FILE_STORAGE = 'config.custom_storages.UploadStorage'
-    AWS_PUBLIC_MEDIA_LOCATION = "media/public"
-
     STATICFILES_STORAGE = 'config.custom_storages.StaticStorage'
-    AWS_PRIVATE_MEDIA_LOCATION = "media/static"
-
     AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
     AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
-    AWS_STORAGE_BUCKET_NAME = "airbnb-clone-joseph"
+    AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
     AWS_DEFAULT_ACL = "public-read"
     AWS_S3_OBJECT_PARAMETERS = {"CacheControl": "max-age=86400"}
     AWS_S3_CUSTOM_DOMAIN = os.environ["AWS_S3_CUSTOM_DOMAIN"]
