7aa625315590c2f8c1d92b7582eb4980323c7dd4
Joseph.P <quep1001@gmail.com>
2022-02-04 17:18:53 +0900

    Connecting try to 'S3 storage'

--- config/settings.py
+++ config/settings.py
@@ -186,7 +186,7 @@ if DEBUG:
     STATICFILES_STORAGE = 'config.custom_storages.StaticStorage'
     AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
     AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
-    AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME")
+    AWS_STORAGE_BUCKET_NAME = "airbnb-clone-joseph"
     AWS_DEFAULT_ACL = "public-read"
     AWS_S3_OBJECT_PARAMETERS = {"CacheControl": "max-age=86400"}
     AWS_S3_CUSTOM_DOMAIN = os.environ.get("AWS_S3_CUSTOM_DOMAIN")
