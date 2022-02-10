f6f6cd714699ba524c436b08400c7823799ee0fb
Joseph.P <quep1001@gmail.com>
2022-02-10 17:16:09 +0900

    Error Test

--- .platform/hooks/predeploy/05_createseed.sh
+++ .platform/hooks/predeploy/05_createseed.sh
@@ -3,8 +3,3 @@
 source /var/app/venv/*/bin/activate
 cd /var/app/staging
 
-python3 manage.py seed_amenities
-python3 manage.py seed_facilities
-python3 manage.py seed_users --number 20
-python3 manage.py seed_rooms --number 40
-python3 manage.py seed_reviews --number 100
